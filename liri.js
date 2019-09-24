0dc2456149f5eebe6b7135bffbf66b77b452e33f
Jason McGowan <jasonmcgowan14@gmail.com>
2019-09-24 15:15:21 -0400

    good to go, wooho

--- liri.js
+++ liri.js
@@ -5,7 +5,6 @@ var Spotify = require("node-spotify-api");
 var axios = require("axios");
 var fs = require("fs");
 var moment = require("moment");
-var dotenv = require("dotenv");
 var spotify = new Spotify(keys.spotify);
 var request = require("request");
 
@@ -27,19 +26,68 @@ var spotifysearch = function(song){
 
 var concertsearch = function(artist){
 
+    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, data) {
+        try {
+            var response = JSON.parse(data)
+            if (response.length != 0) {
+                console.log(`Upcoming concerts include: `)
+                response.forEach(function (element) {
+                    console.log("Venue name: " + element.venue.name);
+                    if (element.venue.country == "United States") {
+                        console.log("City: " + element.venue.city + ", " + element.venue.region);
+                    } else {
+                        console.log("City: " + element.venue.city + ", " + element.venue.country);
+                    }
+                    console.log("Date: " + moment(element.datetime).format('MM/DD/YYYY'));
+                    console.log();
+                })
+            } else {
+                console.log("No upcoming concerts");
+            }
+        }
+        catch (error) {
+            console.log("No upcoming concerts");
+        }
+    });
 }
 
 var moviesearch = function(movie){
-    var queryUrlMovie = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&tomatoes=true&apikey=trilogy";
-    request(queryUrlMovie, function(error, response){
-            
-        
-        console.log('===================================');
-        console.log(response.body)
-        console.log('===================================');
+    axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&tomatoes=true&apikey=trilogy").then(
+    function(response) {
+    
+    console.log('===================================');
+    console.log("Title: " + response.data.Title);
+    console.log("Release year: " + response.data.Year);
+    console.log("The movie's iMDB rating is: " + response.data.imdbRating);
+    console.log("The movie's rotten tomato ratting is: " + response.data.TomatoRating);
+    console.log("The movie is from the: " + response.data.Country);
+    console.log("The movie is avialable in the following languages: " + response.data.Language);
+    console.log("Plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
+    console.log('===================================');
+  });
+}
 
+
+function dowhat() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+      data = data.split(",");
+      var searchtype = data[0]
+      var searchcontent = data[1]
+      switch (searchtype) {
+            case "concert-this":
+                concertsearch(searchcontent)
+            break;
+            case "spotify-this-song":
+                spotifysearch(searchcontent);
+            break;
+            case "movie-this":
+                moviesearch(searchcontent)
+            break;
+      }
     });
 }
+
 
 var searchtype = function(searchtype,searchcontent){
     switch (searchtype){
@@ -52,6 +100,9 @@ var searchtype = function(searchtype,searchcontent){
         case "movie-this":
             moviesearch(searchcontent)
         break;
+        case "do-what-it-says":
+            dowhat()
+        break;
         default: 
             console.log("Not a valid command");
         break;
@@ -60,7 +111,6 @@ var searchtype = function(searchtype,searchcontent){
 
 var search = function(argone,argtwo){
     searchtype(argone,argtwo)
-
 }
 
 
